7b75160f2e1109842b33a664f9b4b5e71c14c1d3
Elliott Woodward <elliottcwoodward@gmail.com>
2019-06-12 16:21:16 -0400

    Remove superfluous if statement.

--- gimli.py
+++ gimli.py
@@ -15,7 +15,6 @@ for submission in submissions:
     for comment in submission.comments.replace_more(limit=100).list():
         if comment.author == "_gimli_bot": continue
         if comment.parent_id[0:3] == 't3_': continue
-        if find_and.match(comment.body):
         if (find_and.match(comment.body)
         and find_and.match(reddit.comment(comment.parent_id[3:]).body)):
             comment.reply(reply)
